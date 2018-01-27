4c841f39e11791a264d4384e0302e34e0bbee6aa
ZKRobi <zkovacs.robert@gmail.com>
2018-01-27 16:40:29 +0100

    Simple Turret

---
+++ GGJ/Assets/Scripts/AutoFire.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoFire : MonoBehaviour
+{
+
+    private Gun gun;
+
+    // Use this for initialization
+    void Start()
+    {
+        gun = this.GetComponent<Gun>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gun.Shoot();
+    }
+}
