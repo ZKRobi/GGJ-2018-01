ee6006027e32010d6120451766c840bc263279dc
ZKRobi <zkovacs.robert@gmail.com>
2018-01-27 20:30:12 +0100

    stuff

---
+++ GGJ/Assets/Scripts/EnemyStates/TriggeredFloortrap.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggeredFloortrap : MonoBehaviour
+{
+
+    public bool isTriggering;
+    public bool isTriggered;
+
+    public float timeToTrigger = 1f;
+    public float resetDelay = 1f;
+
+    private Animator animator;
+
+    // Use this for initialization
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetBool("IsActive", isTriggered);
+        animator.SetBool("IsPreparing", isTriggering);
+    }
+}
