ea3dbf03cadfd57898a778a11e6cb6fbfaef2e6c
Woaf <bfazekas@inf.elte.hu>
2018-01-28 17:21:35 +0100

    Item idle animation

---
+++ GGJ/Assets/ItemIdelFloatingAnimation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ItemIdelFloatingAnimation : MonoBehaviour {
+
+    public Transform _item;
+
+    private void FixedUpdate()
+    {
+        _item.transform.Rotate(0, 10 * (float) Math.Sin(Time.time), 0);
+        _item.transform.Translate(0, 0.220f * (float) Math.Abs(Math.Sin(Time.time)), 0);
+    }
+
+}
